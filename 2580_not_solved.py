00e67e1a900b31e541929a282d876b15ea8bef48
minjun <minjun7410@gmail.com>
2020-12-23 14:32:14 +0000

    20-12-23

--- 2580_not_solved.py
+++ 2580_not_solved.py
@@ -1,74 +1,60 @@
-from collections import deque
 board = [[None for _ in range(9)] for _ in range(9)]
-zero = []
-
+location_lst = []
+zero = 0
 for i in range(9):
     for j, value in enumerate(map(int, input().split())):
         board[i][j] = value
         if value == 0:
-            zero.append((i, j))
-# zero_lst는 진행 상태이다.
-zero_lst = []
+            zero += 1
+fine = False
 
 
 def backtracking():
-    if len(zero_lst) == len(zero):
-        
+    global fine
+    if len(location_lst) == zero:
+        fine = True
         return
-    for i in zero:
-        if i not in zero_lst and check(i):
-            zero_lst.append(i)
-            backtracking()
-            zero_lst.pop()
-
+    for i in range(9):
+        for j in range(9):
+            if not(board[i][j]):
+                number = check((i, j))
+                if (i, j) not in location_lst and number:
+                    location_lst.append((i, j))
+                    origin = board[i][j]
+                    board[i][j] = number
+                    backtracking()
+                    if fine:
+                        return
+                    board[i][j] = origin
+                    location_lst.pop()
+    return
 
 
 def check(location):
     y = location[0]
     x = location[1]
-    # y_lst, x_lst는 해당 리스트에서 필요한 요소들이다
+    lst = list(range(0, 10))
     
-    y_sum = 0
-    y_lst = [i for i in range(1, 10)]
-    for i in range(9):
-        if board[y][i] == 0:
-            y_sum += 1
-        else:
-            y_lst.remove(board[y][i])
-            
-    
-    x_sum = 0
-    x_lst = [i for i in range(1, 10)]
-    for i in range(9):
-        if board[i][x] == 0:
-            x_sum += 1
-        else:
-            x_lst.remove(board[i][x])
-            
-    
-    if y_sum == 1:
-        board[y][x] = y_lst[0]
-        return True
-    if x_sum == 1:
-        board[y][x] = x_lst[0]
-        return True
-    
+    for index in [i for i in range(9)]:
+        lst[board[y][index]] = 0
+        lst[board[index][x]] = 0
+
     div_board_y = (y // 3) * 3
     div_board_x = (x // 3) * 3
 
-    div_sum = 0
-    div = [i for i in range(1, 10)]
     for i in range(3):
         for j in range(3):
-            if board[div_board_y + i][div_board_x + j] == 0:
-                div_sum += 1
-            else:
-                div.remove(board[div_board_y + i][div_board_x + j])
-    if div_sum == 1:
-        board[y][x] = div[0]
-        return True
-    return False
+            lst[board[div_board_y + i][div_board_x + j]] = 0
+    tmp = 0
+    for i in range(10):
+        if lst[i] != 0:
+            tmp = i
+            break
+    return tmp
+
 
 backtracking()
+
 for i in board:
     print(' '.join(map(str, i)))
+
