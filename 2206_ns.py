03cdf8ae612ff39aaf2d2d7b45193e5af50dfd29
minjun <minjun7410@gmail.com>
2021-03-07 07:59:41 +0000

    21-3-7

--- 2206_ns.py
+++ 2206_ns.py
@@ -3,6 +3,11 @@
 # 딱 한번 벽을 부술 수 있다.
 # (1, 1)과 (N, M)에서 시작하는 BFS를 진행한다.
 # 그 다음 모든 벽에 대해 해당 벽이 없을 때 이동거리를 구한다.
+# ------------------------------------------------------
+# 더 효율적인 방법으로
+# 덱에 넣을 인자를 (x, y)만 표현하지 않고 ((x, y), 뚫었었는지 여부) 이렇게 넣으면
+# 도착지점은 최단 시간이 되어 있을 것  (만약 0이라면 도착 X)
+# 주의 사항으로 벽을 뚫었을 때의 2차원 리스트와 뚫지 않았을 때 2차원 리스트를 구분해야한다. (3/7)
 import sys
 from collections import deque
 import copy
@@ -12,45 +17,42 @@ input = sys.stdin.readline
 def bfs(table):
     while queue:
         tmp = queue.popleft()
+        x, y, block = tmp[0][0], tmp[0][1], tmp[1]
         for direction in range(4):
-            nx = dx[direction] + tmp[0]
-            ny = dy[direction] + tmp[1]
-            if nx <= 0 or ny <= 0 or nx > N or ny > M or table[nx][ny] == -1:
+            nx = dx[direction] + x
+            ny = dy[direction] + y
+            if nx <= 0 or ny <= 0 or nx > N or ny > M:
                 continue
-            table[nx][ny] = table[tmp[0]][tmp[1]] + 1
-            queue.append((nx, ny))
+            elif table[nx][ny] == -1 and block == 1:
+                table[nx][ny] = table[x][y] + 1
+                queue.append(((nx, ny), 0))
+                continue
+            elif table[nx][ny] != 0:
+                continue
+            table[nx][ny] = table[x][y] + 1
+            queue.append(((nx, ny), block))
 
 # input
 N, M = map(int, input().split())
 table = [[0 for _ in range(M+1)] for _ in range(N+1)]
-walls = []
 for i in range(N):
     string = input()
     for j in range(M):
         if string[j] == '1':
             table[i][j] = -1
-            walls.append((i, j))
         else:
             table[i][j] = 0
-
-table_copy = copy.deepcopy(table)
 
 # solution
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-queue = deque([(1, 1)])
+queue = deque([((1, 1), 1)])  # ((x, y), 뚫었었는지 여부)
 table[1][1] = 1
 bfs(table)
 
-min_value = 12345678
+print(table)
 
-queue.append[(N, M)]
-table_copy[-1][-1] = 1
-bfs(table_copy)
 
-for wall in walls:
-    x = wall[0]
-    y = wall[1]
     
 
     
