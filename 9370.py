e8d2610a66d4e13cb003302efb5ca591235f48e9
minjun <minjun7410@gmail.com>
2021-04-08 14:59:26 +0000

    21-4-8

--- 9370.py
+++ 9370.py
@@ -5,3 +5,47 @@
 # 2. 이때 그 최단거리가 g와 h 사이를 지나야 한다.
 
 import sys
+import heapq
+
+input = sys.stdin.readline
+INF = 987654321
+
+
+def dijkstra(start):  # 다익스트라 출발지점
+    heap = [(start, 0)]  # (정점, 시작점까지 거리)
+    distance = [INF for _ in range(n+1)]  # 시작점까지의 거리들을 리스트로 정리
+    distance[start] = 0
+    while heap:
+        index, value = heapq.heappop(heap)
+        for v, w in vertices[index]:
+            tmp = value + w
+            if distance[v] > tmp:
+                distance[v] = tmp
+                heapq.heappush(heap, (v, tmp))
+    return distance
+
+
+# input
+for _ in range(int(input())):
+    n, m, t = map(int, input().split())  # 교차로, 도로, 목적지 후보 개수
+    s, g, h = map(int, input().split())  # 출발지, 거점 a, 거점 b
+    vertices = [[] for _ in range(n+1)]  # 간선 리스트
+    for _ in range(m):
+        a, b, d = map(int, input().split())
+        vertices[a].append((b, d))  # (노드, 거리)
+        vertices[b].append((a, d))
+    candidates = sorted([int(input()) for _ in range(t)])  # 후보군 리스트
+    s_dij = dijkstra(s)
+    g_dij = dijkstra(g)
+    h_dij = dijkstra(h)
+    g_h = g_dij[h]
+    s_g = s_dij[g]
+    s_h = s_dij[h]
+    for candidate in candidates:
+        shortest = s_dij[candidate]
+        one = s_g + g_h + h_dij[candidate]
+        two = s_h + g_h + g_dij[candidate]
+        if shortest == min(one, two):
+            print(candidate, end=' ')
+    print()
+    
