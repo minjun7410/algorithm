fc63e58cdc0faeb0fba5f4c8700d09f8ceaf97e6
송민준 <minjun7410@gmail.com>
2023-08-03 00:00:48 +0900

    [D6] Title: [S/W 문제해결 응용] 6일차 - K번째 문자열, Time: 1,102 ms, Memory: 87,032 KB -BaekjoonHub

---
+++ SWEA/D6/1257. ［S／W 문제해결 응용］ 6일차 － K번째 문자열/［S／W 문제해결 응용］ 6일차 － K번째 문자열.java
@@ -0,0 +1,81 @@
+
+import java.util.Scanner;
+
+class Solution {
+    static class TrieNode{
+        int count;
+        boolean isEnd;
+        TrieNode[] nextNodes;
+        public TrieNode(){
+            this.count = 0;
+            this.isEnd = false;
+            this.nextNodes = new TrieNode[26];
+        }
+    }
+    public static TrieNode head;
+    public static void init(){
+        head = new TrieNode();
+    }
+    public static void insert(String str){
+        TrieNode node = head;
+        for(char c : str.toCharArray()){
+            if(node.nextNodes[c - 'a'] == null){
+                node.nextNodes[c-'a'] = new TrieNode();
+            }
+            node.count++;
+            node = node.nextNodes[c - 'a'];
+        }
+        if(node.isEnd){
+            node = head;
+            for(char c : str.toCharArray()) {
+                node.count--;
+                node = node.nextNodes[c-'a'];
+            }
+        }
+        else {
+            node.count++;
+            node.isEnd = true;
+        }
+    }
+    public static void find(int n){
+        TrieNode node = head;
+        if(node.count < n) {
+            System.out.print("none");
+            return;
+        }
+
+        while(true){
+            if(node.isEnd) n--;
+            if(n == 0) return;
+            int remain = n;
+            for(int i = 0; i < 26; i++){
+                n = remain;
+                if(node.nextNodes[i] != null) remain -= node.nextNodes[i].count;
+                if(remain <= 0){
+                    node = node.nextNodes[i];
+                    System.out.print((char)(i+'a'));
+                    break;
+                }
+            }
+
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int TC = sc.nextInt();
+        for(int tc = 1; tc <= TC; tc++){
+            init();
+            int n = sc.nextInt();
+            String str = sc.next();
+            int strLen = str.length();
+            for(int i = 1; i <= strLen; i++){
+                for(int j = 0; j + i <= strLen; j++){
+                    insert(str.substring(j, j+i));
+                }
+            }
+            System.out.printf("#%d ", tc);
+            find(n);
+            System.out.println();
+        }
+    }
+}
